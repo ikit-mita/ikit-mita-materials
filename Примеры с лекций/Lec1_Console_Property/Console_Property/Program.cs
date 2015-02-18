5564d5d819a5f6948f23ea8c8e2fd87f4a45c46c
Denis Zavershinskiy <zaverden@gmail.com>
2015-02-18 14:30:24 +0700

    Пример с первой лекции

---
+++ Примеры с лекций/Lec1_Console_Property/Console_Property/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var staff = new Staff("Ivanov"); //Создаем объект нашего класса.
+            //staff.Fam = "Petrov"; // Мы не имеем возможность изменить фамилию у объекта. Разработчик класса предусмотрительно это запретил, так как данная функциональность требовалась бизнес-правилами.
+
+            Console.WriteLine(staff.Fam); // Но узнать фамилию мы можем.
+            //Console.WriteLine(staff.Salary); // А вот узнать зарплату нет.
+            staff.Salary = 60000; // Мы можем только задать зарплату.
+            staff.Salary = 80000; // Можем изменить ее.
+
+            staff.Age = 30; // Можем задать возраст сотрудника. Теперь он приравняется к 30 годам.
+            staff.Age = 40; // Изменить мы его не сможем. Возраст сотрудника останется равным 30 годам...
+        }
+    }
+}
