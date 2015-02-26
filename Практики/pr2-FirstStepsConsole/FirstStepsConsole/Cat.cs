cb87629eaad0a6016dc064d7402edfca742d92f2
Denis Zavershinskiy <zaverden@gmail.com>
2015-02-26 15:42:52 +0700

    Практика 2

---
+++ Практики/pr2-FirstStepsConsole/FirstStepsConsole/Cat.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStepsConsole
+{
+    public class Cat : Animal
+    {
+        /// <summary>
+        /// В этом поле будет посчитываться, сколько кошек создано в программе
+        /// </summary>
+        public static int Count = 0;
+
+        /// <summary>
+        /// Конструктор без параметров.
+        /// </summary>
+        public Cat()
+            : this("noname", DateTime.Now)
+        {
+            //Перед выполнением логики будет исполнен другой конструктор класса Cat (c 2 параметрами)
+        }
+
+        public Cat(string name, DateTime bd)
+            : base(name, bd)
+        {
+            //Перед выполнением логики будет исполнен конструктор базового класса Animal
+            Count++; //при создании каждой кошки увеличиваем значение поля
+        }
+
+        /// <summary>
+        /// Мяукнуть.
+        /// Закрытый метод.
+        /// </summary>
+        private void Nya()
+        {
+            Console.WriteLine(Name + " мяукает");
+        }
+
+        /// <summary>
+        /// Экземпляр класса Random для получения
+        /// </summary>
+        static Random _rnd = new Random();
+
+        public override void Pet()
+        {
+            if (_rnd.Next(0, 10) < 5)
+            {
+                Nya();
+            }
+            else
+            {
+                Bite();
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Практики/pr2-FirstStepsConsole/FirstStepsConsole/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStepsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cycles();
+            //Animals();
+            Console.ReadLine();
+
+        }
+
+        static void Cycles()
+        {
+            Console.WriteLine("How many bugs?");
+            string countStr = Console.ReadLine();//запрашиваем количество багов в проекте. С консоли вернутс строка
+
+            int count = int.Parse(countStr);//переводим строку в число
+
+            if (count >= 1 && count <= 25)//Если число в интервале [1, 25]
+            {
+                //вывести в цикле «Было в проекте N багов, один исправили, осталось N-1 багов»
+                for (int i = 0; i < count; i++)
+                {
+                    var s = string.Format("Было в проекте {0} багов, один исправили, осталось {1} багов", count - i, count - i - 1);
+                    Console.WriteLine(s);
+                }
+            }
+            else if (count < 1) //Если число меньше 1 – вывести строку «Плохо тестируете»
+            {
+                Console.WriteLine("Bad testing");
+            }
+            else //В остальных случаях – «Проект обречен, попробуйте стать врачем»
+            {
+                Console.WriteLine("Project is failed");
+            }
+        }
+
+        static void Animals()
+        {
+            //проверяем, что счетчики по нулям
+            Console.WriteLine(Cat.Count);
+            Console.WriteLine(Animal.Count);
+
+            //создаем кошку. проверяем, что счетчики увеличились
+            var cat = new Cat();
+            Console.WriteLine(Animal.Count);
+            Console.WriteLine(Cat.Count);
+
+            //создаем кошку другим конструктором. проверяем, что счетчики увеличились
+            var cat1 = new Cat("Cat", new DateTime(1999, 1, 1));
+            Console.WriteLine(Animal.Count);
+            Console.WriteLine(Cat.Count);
+
+            //создаем кошку. проверяем, что счетчик животных увеличисся, счетчик кошек не изменился
+            var dog = new Dog();
+            Console.WriteLine(Animal.Count);
+            Console.WriteLine(Cat.Count);
+
+            //гладим кошку, чтобы проверить, что исправление логики генерации случайных чисел сработала
+            for (int i = 0; i < 10; i++)
+            {
+                cat1.Pet();
+            }
+
+            //Проверяем, что перегруженный метод возвращает разные значения для разных классов.
+            Console.WriteLine(cat.WhatYouSay());
+            Console.WriteLine(dog.WhatYouSay());
+        }
+    }
+}
