6b56e06d6df18e0484b68d00a8672ce44f54b20f
Denis Zavershinskiy <zaverden@gmail.com>
2015-03-27 18:34:51 +0700

    практика 14

---
+++ Практики/pr14-TenantsManager/TendantsManager.Model/TenantRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenantsManager.Model
+{
+    public class TenantRepository : MemoryRepository<Tenant>
+    {
+        public TenantRepository()
+        {
+            Add(new Tenant
+            {
+                Birthday = new DateTime(1990, 6, 1),
+                Debt = 50.5,
+                IsDeleted = false,
+                Name = "Ivanov Ivan"
+            });
+            Add(new Tenant
+            {
+                Birthday = new DateTime(1992, 4, 11),
+                Debt = 350.5,
+                IsDeleted = false,
+                Name = "Ivanov1 Ivan1"
+            });
+            Add(new Tenant
+            {
+                Birthday = new DateTime(1978, 3, 23),
+                Debt = 250.5,
+                IsDeleted = true,
+                Name = "Ivanov2 Ivan2"
+            });
+            Add(new Tenant
+            {
+                Birthday = new DateTime(1990, 6, 15),
+                Debt = 150.5,
+                IsDeleted = false,
+                Name = "Ivanov3 Ivan3"
+            });
+        }
+    }
+}
