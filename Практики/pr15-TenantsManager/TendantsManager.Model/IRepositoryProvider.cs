100e4d9428a5129e21bb4dbd078380b49fe385b8
Denis Zavershinskiy <zaverden@gmail.com>
2015-03-31 17:49:53 +0700

    copy lab 14

---
+++ Практики/pr15-TenantsManager/TendantsManager.Model/IRepositoryProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TenantsManager.Model
+{
+    public interface IRepositoryProvider : IDisposable
+    {
+        IRepository<T> GetRepository<T>() where T : class, IDomainObject;
+        void SaveChanges();
+    }
+}
---
+++ Практики/pr15-TenantsManager/TendantsManager.Model/TenantsContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace TenantsManager.Model
+{
+    public class TenantsContext : DbContext
+    {
+        public TenantsContext()
+            : base("TenantsConnection") { }
+
+        public IDbSet<Tenant> Tenants { get; set; }
+    }
+}
