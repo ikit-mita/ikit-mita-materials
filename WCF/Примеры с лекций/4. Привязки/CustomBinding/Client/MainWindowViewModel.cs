63e043d2189092d7f39957d219d0c15f1e9c7774
denis-tsv <yadeniskotcvet@gmail.com>
2015-05-08 11:35:59 +0700

    custom binding

---
+++ WCF/Примеры с лекций/4. Привязки/CustomBinding/Client/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace Client
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _data;
+        private ICommand _getDataCommand;
+
+        public string Data
+        {
+            get { return _data; }
+            set { SetProperty(ref _data, value); }
+        }
+
+        public ICommand GetDataCommand
+        {
+            get { return _getDataCommand ?? (_getDataCommand = new DelegateCommand(GetData)); }
+        }
+
+        private async void GetData()
+        {
+            var serviceClient = new TestServiceReference.TestServiceClient();
+            
+            var data = await serviceClient.GetDataAsync();
+
+            Data = data;
+        }
+    }
+}
---
+++ WCF/Примеры с лекций/4. Привязки/CustomBinding/Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = new ServiceHost(typeof(TestService));
+            serviceHost.Open();
+            
+            Console.WriteLine("Для завершения нажмите <ENTER>.\n\n");
+            Console.ReadLine();
+            
+            serviceHost.Close();
+        }
+    }
+}
